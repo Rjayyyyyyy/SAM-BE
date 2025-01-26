80f9764e646af2181279ce471ddd0b817060afa8
raymond <mariathaliaquinn@gmail.com>
2025-01-26 19:29:26 +0800

    changed

--- FO1/admin/index.php
+++ FO1/admin/index.php
@@ -13,7 +13,7 @@ require_once '../connect.php';
     <title>Olympics Events Admin Portal</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-    <link rel="icon" type="image/png" href="images/logo.png">
+    <link rel="icon" type="image/png" href="icon/logo.png">
 
 </head>
 <style>
